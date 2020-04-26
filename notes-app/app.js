c7d16b01847c0e3704e7a07019d43d532ed05a5a
Sara Alkhamri <s.alkhamri@gmail.com>
2020-04-25 21:40:22 -0700

    Used the fs writefilesync module to create notes.txt in notes-app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'Hey, this is Sara')
+// fs.writeFileSync('notes.txt', 'Hey, this is Sara')
+
+fs.appendFileSync('notes.txt', ' one more test!')
